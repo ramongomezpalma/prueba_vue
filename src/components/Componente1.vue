218db1894c04d8cdfc31dc91190033457d7b97b2
ramongomezpalma <ramon.gomez.palma@gmail.com>
2024-01-09 15:24:09 -0400

    'ini'

---
+++ src/components/Componente1.vue
@@ -0,0 +1,38 @@
+<template>
+<h1>{{ msg }} de {{ nombre }}</h1>
+<p>
+    Este es mi primer componente.
+</p>
+</template>
+
+<script>
+export default {
+    name: 'ComponentModulo1',
+    props: {
+        msg: String,
+        nombre: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
